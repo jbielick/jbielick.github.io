c9853e5206bffe79094ce7c35a6607f1cf87fab7
Josh Bielick <jbielick@gmail.com>
2017-12-08 19:26:14 -0500

    empty dir

--- app.js
+++
@@ -1,8 +0,0 @@
-var Kona = require('kona');
-var app = new Kona({root: __dirname});
-
-require('dotenv').load();
-
-app.initialize().on('ready', function() {
-  this.listen();
-});
--- config/routes.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(router) {
-
-  router.get('/').to('main.home');
-  router.match('/pages/*path', 'GET').to('main.show');
-  router.get('/grams').to('main.grams');
-
-}
