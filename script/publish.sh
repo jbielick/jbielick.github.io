b3e9a796aa0b71257af28bd4333c34ab1af716e2
Josh Bielick <jbielick@gmail.com>
2017-12-08 23:08:53 -0500

    rename deploy script

--- script/publish.sh
+++ script/publish.sh
@@ -16,16 +16,16 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+git submodule update
+
+echo "Checking out master branch into public"
+git worktree add -B master public origin/master
 
 echo "Removing existing files"
 rm -rf public/*
 
-git submodule update
-
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
