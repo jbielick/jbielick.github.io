d453a0a3f084cde41800b9eae46975faa5133e08
Joshua Bielick <josh@geosh.local>
2024-05-10 10:22:29 -0400

    update

--- script/publish.sh
+++ script/publish.sh
@@ -31,5 +31,7 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating master branch"
-cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
+echo "Updating and pushing master branch"
+cd public && git add --all && git commit -m "Publishing to master (publish.sh)" && git push origin
+
+echo "Done!"
